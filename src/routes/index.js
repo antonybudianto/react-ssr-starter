938f71b26535dc1cd6478cb38bc6673a60baa2e1
Antony Budianto <antonybudianto@gmail.com>
2018-05-03 12:48:30 +0700

    refactor util

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import { UAParser } from 'ua-parser-js'
 
 import HomeView from './Home'
 import NotFoundPage from './NotFoundPage'
-// import { fetchUser } from '../reducers/user'
 import { initUserAgent } from '../reducers/user-agent'
 
 const routes = [{ ...HomeView }, { ...NotFoundPage }]
@@ -15,9 +14,6 @@ export const getInitialData = (req, store) => {
   store.dispatch(initUserAgent(ua))
   return matchRoutes(routes, path).map(({ route }) => {
     const promises = []
-    // if (route.auth) {
-    //   promises.push(store.dispatch(fetchUser(store.cookies)))
-    // }
     if (route.loadData) {
       promises.push(route.loadData(store, req))
     }
