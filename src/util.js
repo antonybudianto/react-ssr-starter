a61f0e82da6216189369bf6e3551f5399ac18d89
Antony Budianto <antonybudianto@gmail.com>
2018-05-03 11:31:43 +0700

    remove deadcode

--- src/util.js
+++ src/util.js
@@ -13,13 +13,3 @@ export const fetch = async (url, headers) => {
     throw err.response
   }
 }
-
-export const roundTo = (number, precision) => {
-  let factor = Math.pow(10, precision)
-  return Math.trunc(number * factor) / factor
-}
-
-export const STATUS_CODE = {
-  C500: '500000',
-  C200: '200000'
-}
