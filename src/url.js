cd40b8fb138df8f7e7825188f8ffe19004e750d5
Antony Budianto <antonybudianto@gmail.com>
2018-05-08 23:19:26 +0700

    fix url

--- src/url.js
+++ src/url.js
@@ -1,2 +1,12 @@
+import { ASSET_URL_PROD, ASSET_URL_STAG } from './constant'
+
+let assetUrl = ASSET_URL_PROD
+
+if (__DEV__) {
+  assetUrl = ASSET_URL_DEV
+} else if (__STAG__) {
+  assetUrl = ASSET_URL_STAG
+}
+
 export const HOME_PATH = '/'
-export const ASSET_URL = '/'
+export const ASSET_URL = assetUrl
