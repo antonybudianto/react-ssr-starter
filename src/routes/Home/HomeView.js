d9d744aa787d82f393445c9e002751791762ab63
Antony Budianto <antonybudianto@gmail.com>
2018-05-10 15:44:29 +0700

    add more example for redux ssr

--- src/routes/Home/HomeView.js
+++ src/routes/Home/HomeView.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
+import { toggleLogin } from '../../reducers/user'
 import './HomeView.scss'
 import TestView from './TestView'
 
 class HomeView extends Component {
+  static propTypes = {
+    user: PropTypes.object,
+    toggleLogin: PropTypes.func
+  }
+
   render() {
     return (
       <div className="home-view">
         <div>
-          home view (change HomeView.js and the count should not be reset)
+          home antony (change HomeView.js and the count should not be reset)
+        </div>
+        <div>
+          user: {this.props.user.isLoggedIn ? 'loggedIn' : 'notLoggedIn'}
+        </div>
+        <div>
+          <button onClick={this.props.toggleLogin}>toggle login</button>
         </div>
         <TestView />
       </div>
@@ -16,4 +30,12 @@ class HomeView extends Component {
   }
 }
 
-export default HomeView
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+const mapDispatchToProps = {
+  toggleLogin
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeView)
