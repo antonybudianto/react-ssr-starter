3dc810f9e6266cf63d4f0f5d6e157129c3219067
Antony Budianto <antonybudianto@gmail.com>
2018-05-26 21:45:12 +0700

    refactor: use globals instead of hardcoded env

--- config/env.config.js
+++ config/env.config.js
@@ -1,11 +1,11 @@
 import { ASSET_URL_DEV, ASSET_URL_PROD, ASSET_URL_STAG } from '../src/constant'
+import config from './project.config'
 
 let assetUrl = ASSET_URL_DEV
-const env = process.env.NODE_ENV
 
-if (env === 'staging') {
+if (config.globals.__STAG__) {
   assetUrl = ASSET_URL_STAG
-} else if (env === 'production') {
+} else if (config.globals.__PROD__) {
   assetUrl = ASSET_URL_PROD
 }
 
