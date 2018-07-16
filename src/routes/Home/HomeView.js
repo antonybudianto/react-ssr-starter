5fedd11485c5000940680e1522c68127b6ffab0d
Antony Budianto <antonybudianto@gmail.com>
2018-07-16 09:04:20 +0700

    implement simple form

--- src/routes/Home/HomeView.js
+++ src/routes/Home/HomeView.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Formik } from 'formik'
+import validator from 'validator'
 
 import { toggleLogin } from '../../reducers/user'
 import './HomeView.scss'
-import TestView from './TestView'
+// import TestView from './TestView'
+
+const initialValue = {
+  email: '',
+  password: ''
+}
+
+const ErrorInput = ({ errors, touched, field }) =>
+  (errors[field] &&
+    touched[field] && (
+      <small
+        style={{
+          color: 'orangered'
+        }}
+      >
+        {errors[field]}
+      </small>
+    )) ||
+  null
 
 class HomeView extends Component {
   static propTypes = {
@@ -12,19 +32,85 @@ class HomeView extends Component {
     toggleLogin: PropTypes.func
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      ready: false
+    }
+    this.handleValidation = this.handleValidation.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.renderForm = this.renderForm.bind(this)
+  }
+
+  handleValidation(values) {
+    const errors = {}
+
+    const { email, password } = values
+
+    if (email === '') {
+      errors.email = 'Email harus diisi'
+    } else if (!validator.isEmail(email)) {
+      errors.email = 'Email tidak valid'
+    }
+
+    if (password === '') {
+      errors.password = 'Password harus diisi'
+    }
+
+    return errors
+  }
+
+  handleSubmit(form) {
+    console.log(form)
+    this.setState({
+      ready: true
+    })
+  }
+
+  renderForm(props) {
+    const { handleSubmit, handleChange, handleBlur, errors, touched } = props
+
+    return (
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="my1">
+          <label className="pr1">Email</label>
+          <input
+            name="email"
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <ErrorInput errors={errors} touched={touched} field="email" />
+        </div>
+        <div className="my1">
+          <label className="pr1">Password</label>
+          <input
+            name="password"
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <ErrorInput errors={errors} touched={touched} field="password" />
+        </div>
+        <div>
+          <button className="p1" type="submit">
+            Submit
+          </button>
+        </div>
+        <div>{this.state.ready && 'Success'}</div>
+      </form>
+    )
+  }
+
   render() {
     return (
       <div className="home-view">
-        <div>
-          home view (change HomeView.js and the count should not be reset)
-        </div>
-        <div>
-          user: {this.props.user.isLoggedIn ? 'loggedIn' : 'notLoggedIn'}
-        </div>
-        <div>
-          <button onClick={this.props.toggleLogin}>toggle login</button>
-        </div>
-        <TestView />
+        <Formik
+          initialValues={initialValue}
+          validate={this.handleValidation}
+          onSubmit={this.handleSubmit}
+          render={this.renderForm}
+        />
       </div>
     )
   }
